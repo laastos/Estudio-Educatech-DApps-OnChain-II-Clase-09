66edea54282d14d6642ded8cdd28ce86e4013223
Libardo Arroyave <laastos@gmail.com>
2024-06-27 19:28:28 -0500

    WIP

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,9 +1,32 @@
-export default function handler(req : any, res : any) {
+import { verifyMessage } from "ethers";
+
+export default function handler(req: any, res: any) {
   if (req.method === 'GET') {
-    
-    res.status(200).json({ message: 'This is a GET request' });
+    const { request } = req.query;
+    if (request === 'nonce') {
+      res.status(200).json({
+        message: 'This is a GET request for nonce',
+        nonce: 'Get unique nonce 123456'
+      });
+      return;
+    } else {
+      res.status(200).json({ message: 'This is a GET request' });
+      return;
+    }
   } else if (req.method === 'POST') {
-    
-    res.status(200).json({ message: 'This is a POST request' });
+    const { method, address, nonce, signature } = req.body;
+    if (method === 'signature') {
+      const addressFormatted = address.toLowerCase();
+      const verifyAddress = verifyMessage(nonce, signature);
+      if (verifyAddress.toLowerCase() === addressFormatted) {
+        res.status(200).json(
+          {
+            message: 'Signature verified',
+            success: true,
+            token: 'This is a JWT token'
+          }
+        );
+      }
+    }
   }
 }
